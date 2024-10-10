544bea687e5cfe9402270980095b87f4e456d195
aleme79 <alemetroid770@gmail.com>
2024-10-10 17:46:05 +0200

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		
+		Scanner p = new Scanner(System.in);
+		System.out.println("Tu nombre de usuario");
+		String nombre = p.nextLine(); 
+		System.out.println("Dime tu apellido");
+		String apellido = p.nextLine();
+		System.out.println();
+		
+		
+		
+	}
+	
+}
